945cbc0f4a7a8f9dc7ffdd4aeabdc3891a5635a7
samwiseg0 <samwiseg0@gmail.com>
2020-08-24 18:53:32 -0400

    Add the ability to change what httpd is listening on easier

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ from stagg_ekg_plus import StaggEKG
 
 PORT = 8000
 MAC = "00:1C:97:18:7B:04"
+LISTEN_ON = "0.0.0.0"
 
 class web_server(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -59,6 +60,7 @@ def convert_c_to_f(temp):
 
 # Allow reuse of the socket after killing httpd
 socketserver.TCPServer.allow_reuse_address = True
+with socketserver.TCPServer((LISTEN_ON, PORT), web_server) as httpd:
     # httpd.serve_forever()
     print("STARTED")
     try:
